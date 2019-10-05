7d4e2dee68a40f287418d4ca699a714f80fbcb71
Harry <harryaberg@gmail.com>
2019-10-05 18:24:58 +0100

    first bundle with c1 and c2

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export const c1 = () => {
+    return (
+        <div>
+            component 1
+        </div>
+    )
+}
+
+export const c2 = () => {
+    return (
+        <div>
+            component 2
+        </div>
+    )
+}
