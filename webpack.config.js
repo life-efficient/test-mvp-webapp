0a8d1a0085f903386a338efc8ec716a574f3ca34
Harry <harryaberg@gmail.com>
2019-10-06 10:44:14 +0100

    deploying latest

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 var path = require('path');
 // var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 module.exports = {
     mode: "production",
@@ -11,23 +14,59 @@ module.exports = {
     },
     module: {
         rules: [
-        {
-            test: /\.js$/,
-            include: path.resolve(__dirname, 'src'),
-            exclude: /(node_modules|bower_components|build)/,
-            use: {
-            loader: 'babel-loader'
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /(node_modules|bower_components|build)/,
+                use: {
+                loader: 'babel-loader'
+                }
+            }, 
+            {
+                test: /\.module\.s(a|c)ss$/,
+                loader: [
+                isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {
+                    modules: true,
+                    sourceMap: isDevelopment
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                    sourceMap: isDevelopment
+                }
             }
-        }, {
-            test: /\.*css$/,
-            use : [
-                    'css-loader',
-                    'sass-loader'
-                ]
-        },
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      }
         ]
     },
     externals: {
         'react': 'commonjs react' 
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss']
     }
 };
